d0411e433324d207189bbeb4b1f104ced6048c08
Akila_Ayanthi <aayanthi@gmail.com>
2021-11-21 20:50:51 +0530

    edited for inferencing

--- models.py
+++ models.py
@@ -3,6 +3,10 @@ from torch import nn
 import torch.nn.functional as F
 from tool.torch_utils import *
 from tool.yolo_layer import YoloLayer
+from tool.utils import load_class_names, plot_boxes_cv2
+from tool.torch_utils import do_detect
+import pathlib
+from fnmatch import fnmatch
 
 
 class Mish(torch.nn.Module):
@@ -454,56 +458,85 @@ if __name__ == "__main__":
     import cv2
 
     namesfile = None
-    if len(sys.argv) == 6:
+    if len(sys.argv) == 5:
         n_classes = int(sys.argv[1])
         weightfile = sys.argv[2]
-        imgfile = sys.argv[3]
-        height = int(sys.argv[4])
-        width = int(sys.argv[5])
-    elif len(sys.argv) == 7:
+        # imgfile = sys.argv[3]
+        height = int(sys.argv[3])
+        width = int(sys.argv[4])
+    elif len(sys.argv) == 6:
         n_classes = int(sys.argv[1])
         weightfile = sys.argv[2]
-        imgfile = sys.argv[3]
-        height = sys.argv[4]
-        width = int(sys.argv[5])
-        namesfile = int(sys.argv[6])
+        # imgfile = sys.argv[3]
+        height = sys.argv[3]
+        width = int(sys.argv[4])
+        namesfile = int(sys.argv[5])
     else:
         print('Usage: ')
         print('  python models.py num_classes weightfile imgfile namefile')
 
-    model = Yolov4(yolov4conv137weight=None, n_classes=n_classes, inference=True)
-
+    model = Yolov4(yolov4conv137weight=None, n_classes=80, inference=True)
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if torch.cuda.device_count() > 1:
+        model = torch.nn.DataParallel(model)
+    model.to(device=device)
     pretrained_dict = torch.load(weightfile, map_location=torch.device('cuda'))
     model.load_state_dict(pretrained_dict)
+    model.cuda()
 
     use_cuda = True
     if use_cuda:
         model.cuda()
 
-    img = cv2.imread(imgfile)
+    root = "/home/dissana8/LAB/Visor/cam1"
+    files=[]
+    pattern = "*.jpg"
+
+    f = open("cam1_paths.txt", "a")
+    for path, subdirs, files in os.walk(root):
+        for name in files:
+            if fnmatch(name, pattern):
+                f.write(os.path.join(path, name)+"\n")
+    f.close()
+
+    f = open("cam1_paths.txt", "r")
+    files = f.readlines()
+    for i in range(2):
+        imgfile = files[i].strip('\n')
+        img = cv2.imread(imgfile)
 
     # Inference input size is 416*416 does not mean training size is the same
     # Training size could be 608*608 or even other sizes
     # Optional inference sizes:
     #   Hight in {320, 416, 512, 608, ... 320 + 96 * n}
     #   Width in {320, 416, 512, 608, ... 320 + 96 * m}
-    sized = cv2.resize(img, (width, height))
-    sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+        sized = cv2.resize(img, (width, height))
+        sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
 
-    from tool.utils import load_class_names, plot_boxes_cv2
-    from tool.torch_utils import do_detect
+    
 
-    for i in range(2):  # This 'for' loop is for speed check
+        for i in range(2):  # This 'for' loop is for speed check
                         # Because the first iteration is usually longer
-        boxes = do_detect(model, sized, 0.4, 0.6, use_cuda)
+            boxes = do_detect(model, sized, 0.4, 0.6, use_cuda)
 
-    if namesfile == None:
-        if n_classes == 20:
-            namesfile = 'data/voc.names'
-        elif n_classes == 80:
-            namesfile = 'data/coco.names'
-        else:
-            print("please give namefile")
+        if namesfile == None:
+            if n_classes == 20:
+                namesfile = '/home/dissana8/pytorch-YOLOv4/data/voc.names'
+            elif n_classes == 80:
+                namesfile = '/home/dissana8/pytorch-YOLOv4/data/coco.names'
+            else:
+                print("please give namefile")
 
-    class_names = load_class_names(namesfile)
-    plot_boxes_cv2(img, boxes[0], 'predictions.jpg', class_names)
+        class_names = load_class_names(namesfile)
+
+        real_count=0
+        imgfile = imgfile.split('\\')[6:]
+        imgname = '/'.join(imgfile)
+        p_count = plot_boxes_cv2(img, boxes[0], imgname, class_names)
+        print("Number of people detected:", p_count)
+
+        gt= np.load('/home/dissana8/LAB/data/LAB/cam1_coords.npy', allow_pickle=True)
+        for i in range(len(gt)):
+            if gt[i][0] == imgname:
+                real_count+=1
+        print("Number of people in ground truth :", real_count)
--- tool/utils.py
+++ tool/utils.py
@@ -122,23 +122,33 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
             rgb = color
         else:
             rgb = (255, 0, 0)
-        if len(box) >= 7 and class_names:
-            cls_conf = box[5]
-            cls_id = box[6]
+        # if len(box) >= 7 and class_names:
+        #     cls_conf = box[5]
+        #     cls_id = box[6]
+        #     print('%s: %f' % (class_names[cls_id], cls_conf))
+        #     classes = len(class_names)
+        #     offset = cls_id * 123457 % classes
+        #     red = get_color(2, offset, classes)
+        #     green = get_color(1, offset, classes)
+        #     blue = get_color(0, offset, classes)
+        #     if color is None:
+        #         rgb = (red, green, blue)
+        #     img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
+        # img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+
+        count=0
+        cls_conf = box[5]
+        cls_id = box[6]
+        if str(class_names[cls_id])=='person':
+            count+=1
             print('%s: %f' % (class_names[cls_id], cls_conf))
-            classes = len(class_names)
-            offset = cls_id * 123457 % classes
-            red = get_color(2, offset, classes)
-            green = get_color(1, offset, classes)
-            blue = get_color(0, offset, classes)
-            if color is None:
-                rgb = (red, green, blue)
             img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
-        img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+            img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 1)
+
     if savename:
         print("save plot results to %s" % savename)
         cv2.imwrite(savename, img)
-    return img
+    return img, count
 
 
 def read_truths(lab_path):
