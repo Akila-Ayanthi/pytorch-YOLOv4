ed3560e0dd155374dea8af5f16aa89f27dd85efc
Akila_Ayanthi <aayanthi@gmail.com>
2021-11-30 15:28:18 +0530

    bbox

--- models.py
+++ models.py
@@ -567,11 +567,11 @@ if __name__ == "__main__":
         for k in range(len(gt)): 
             if gt[k][0] == imgname:
 #                 print(imgname)
-                #print(gt[k])
+                print(gt[k])
                 box = [float(gt[k][1]), float(gt[k][2]), 40, 80]
                 box = torch.tensor(box)
                 bbox = box_center_to_corner(box)
-                print(bbox)
+#                 print(bbox)
                     
 #                 img = cv2.rectangle(img, (int(bbox[0].item()), int(bbox[1].item())), (int(bbox[2].item()), int(bbox[3].item())), (0,255,0), 1)
 # #                 a.add_patch(bbox_to_rect(bbox, 'red')) 
