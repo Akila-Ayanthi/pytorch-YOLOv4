31fc18dd833a64e2d5458b6f522433522bcae4af
Akila_Ayanthi <aayanthi@gmail.com>
2021-11-30 13:26:18 +0530

    bbox

--- models.py
+++ models.py
@@ -571,7 +571,7 @@ if __name__ == "__main__":
                 box = [float(gt[k][1]), float(gt[k][2]), 40, 80]
                 box = torch.tensor(box)
                 bbox = box_center_to_corner(box)
-                img = cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), rgb, 1)
+                img = cv2.rectangle(img, (bbox[0], bbox[1]), (bbox[2], bbox[3]), (0,255,0), 1)
 #                 a.add_patch(bbox_to_rect(bbox, 'red')) 
                 print("added")
         #         real_count+=1
@@ -582,10 +582,12 @@ if __name__ == "__main__":
         directory = '/home/dissana8/pytorch-YOLOv4/output/'+imgname.split('/')[0]
         if not os.path.exists(directory):
             os.makedirs(directory)
+            
+        break
         
-        plt.axis("off")
-        plt.savefig("/home/dissana8/pytorch-YOLOv4/output/"+imgname)
-        plt.show()
+#         plt.axis("off")
+#         plt.savefig("/home/dissana8/pytorch-YOLOv4/output/"+imgname)
+#         plt.show()
         
         
 
