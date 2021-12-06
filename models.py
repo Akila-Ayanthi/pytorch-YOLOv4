576bc6e0497b1a634e73bd2f04a024390878d8c7
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-06 10:38:03 +0530

    path updated

--- models.py
+++ models.py
@@ -471,7 +471,7 @@ def findClosest(time, camera_time_list):
     val = min(camera_time_list, key=lambda x: abs(x - time))
     return camera_time_list.index(val)
 
-def extract_frames(path,file_name, model, class_names, width, height):
+def extract_frames(path,file_name, model, class_names, width, height, gt1, gt2, gt3, gt4):
     #===== process the index files of camera 1 ======#
     with open('/home/dissana8/LAB/Visor/cam1/index.dmp') as f:
         content = f.readlines()
