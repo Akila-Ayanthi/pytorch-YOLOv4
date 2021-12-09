a90535290d476405a64e59c514a1b84752c4f73e
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-09 15:24:00 +0530

    iou calculation

--- models.py
+++ models.py
@@ -476,105 +476,106 @@ def custom_bbox(gt_coords, img, imgname):
                 
     return img, cbbox_coords
 
-# def get_iou(a, b, epsilon=1e-5):
-#     """ Given two boxes `a` and `b` defined as a list of four numbers:
-#             [x1,y1,x2,y2]
-#         where:
-#             x1,y1 represent the upper left corner
-#             x2,y2 represent the lower right corner
-#         It returns the Intersect of Union score for these two boxes.
-
-#     Args:
-#         a:          (list of 4 numbers) [x1,y1,x2,y2]
-#         b:          (list of 4 numbers) [x1,y1,x2,y2]
-#         epsilon:    (float) Small value to prevent division by zero
-
-#     Returns:
-#         (float) The Intersect of Union score.
-#     """
-
-#     print(a)
-#     print(b)
-#     # COORDINATES OF THE INTERSECTION BOX
-#     # x1 = max(a[0], b[0])
-#     # y1 = max(a[1], b[1])
-#     # x2 = min(a[2], b[2])
-#     # y2 = min(a[3], b[3])
-
-#     x1 = max(a[0], b[0])
-#     y1 = max(a[1], b[1])
-#     x2 = min(a[2], b[2])
-#     y2 = min(a[3], b[3])
-
-
-
-#     # AREA OF OVERLAP - Area where the boxes intersect
-#     width = (x2 - x1)
-#     height = (y2 - y1)
-#     print(width)
-#     print(height)
-#     # handle case where there is NO overlap
-#     if (width<0) or (height <0):
-#         return 0.0
-#     area_overlap = width * height
-
-#     # COMBINED AREA
-#     area_a = (a[2] - a[0]) * (a[3] - a[1])
-#     area_b = (b[2] - b[0]) * (b[3] - b[1])
-#     area_combined = area_a + area_b - area_overlap
-
-#     # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
-#     iou = area_overlap / (area_combined+epsilon)
-#     return iou
-
-def batch_iou(a, b, epsilon=1e-5):
-    """ Given two arrays `a` and `b` where each row contains a bounding
-        box defined as a list of four numbers:
+def get_iou(a, b, epsilon=1e-5):
+    """ Given two boxes `a` and `b` defined as a list of four numbers:
             [x1,y1,x2,y2]
         where:
             x1,y1 represent the upper left corner
             x2,y2 represent the lower right corner
-        It returns the Intersect of Union scores for each corresponding
-        pair of boxes.
+        It returns the Intersect of Union score for these two boxes.
 
     Args:
-        a:          (numpy array) each row containing [x1,y1,x2,y2] coordinates
-        b:          (numpy array) each row containing [x1,y1,x2,y2] coordinates
+        a:          (list of 4 numbers) [x1,y1,x2,y2]
+        b:          (list of 4 numbers) [x1,y1,x2,y2]
         epsilon:    (float) Small value to prevent division by zero
 
     Returns:
-        (numpy array) The Intersect of Union scores for each pair of bounding
-        boxes.
-
+        (float) The Intersect of Union score.
     """
 
-    print(a[:, 0])
-    # print("b")
-    # print(b)
-    # COORDINATES OF THE INTERSECTION BOXES
-    x1 = np.array([a[:, 0], b[:, 0]]).max(axis=0)
-    y1 = np.array([a[:, 1], b[:, 1]]).max(axis=0)
-    x2 = np.array([a[:, 2], b[:, 2]]).min(axis=0)
-    y2 = np.array([a[:, 3], b[:, 3]]).min(axis=0)
+    print(a)
+    print(b)
+    iou = 0.0
 
-    # AREAS OF OVERLAP - Area where the boxes intersect
-    width = (x2 - x1)
-    height = (y2 - y1)
+    for i in range(len(b)):
+    # COORDINATES OF THE INTERSECTION BOX
+        x1 = max(a[i][0], b[i][0])
+        y1 = max(a[i][1], b[i][1])
+        x2 = min(a[i][2], b[i][2])
+        y2 = min(a[i][3], b[i][3])
 
-    # handle case where there is NO overlap
-    width[width < 0] = 0
-    height[height < 0] = 0
 
-    area_overlap = width * height
 
-    # COMBINED AREAS
-    area_a = (a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1])
-    area_b = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
-    area_combined = area_a + area_b - area_overlap
+    # AREA OF OVERLAP - Area where the boxes intersect
+        width = (x2 - x1)
+        height = (y2 - y1)
+        print(width)
+        print(height)
+        # handle case where there is NO overlap
+        if (width<0) or (height <0):
+            iou = 0.0
+        area_overlap = width * height
 
-    # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
-    iou = area_overlap / (area_combined + epsilon)
+    # COMBINED AREA
+        area_a = (a[i][2] - a[i][0]) * (a[i][3] - a[i][1])
+        area_b = (b[i][2] - b[i][0]) * (b[i][3] - b[i][1])
+        area_combined = area_a + area_b - area_overlap
+
+        # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+        n_iou = area_overlap / (area_combined+epsilon)
+        if iou <= n_iou:
+            iou = n_iou
     return iou
+        
+
+# def batch_iou(a, b, epsilon=1e-5):
+#     """ Given two arrays `a` and `b` where each row contains a bounding
+#         box defined as a list of four numbers:
+#             [x1,y1,x2,y2]
+#         where:
+#             x1,y1 represent the upper left corner
+#             x2,y2 represent the lower right corner
+#         It returns the Intersect of Union scores for each corresponding
+#         pair of boxes.
+
+#     Args:
+#         a:          (numpy array) each row containing [x1,y1,x2,y2] coordinates
+#         b:          (numpy array) each row containing [x1,y1,x2,y2] coordinates
+#         epsilon:    (float) Small value to prevent division by zero
+
+#     Returns:
+#         (numpy array) The Intersect of Union scores for each pair of bounding
+#         boxes.
+
+#     """
+
+#     # print(a[:, 0])
+#     # # print("b")
+#     # # print(b)
+#     # # COORDINATES OF THE INTERSECTION BOXES
+#     x1 = np.array([a[:, 0], b[:, 0]]).max(axis=0)
+#     y1 = np.array([a[:, 1], b[:, 1]]).max(axis=0)
+#     x2 = np.array([a[:, 2], b[:, 2]]).min(axis=0)
+#     y2 = np.array([a[:, 3], b[:, 3]]).min(axis=0)
+
+#     # AREAS OF OVERLAP - Area where the boxes intersect
+#     width = (x2 - x1)
+#     height = (y2 - y1)
+
+#     # handle case where there is NO overlap
+#     width[width < 0] = 0
+#     height[height < 0] = 0
+
+#     area_overlap = width * height
+
+#     # COMBINED AREAS
+#     area_a = (a[:, 2] - a[:, 0]) * (a[:, 3] - a[:, 1])
+#     area_b = (b[:, 2] - b[:, 0]) * (b[:, 3] - b[:, 1])
+#     area_combined = area_a + area_b - area_overlap
+
+#     # RATIO OF AREA OF OVERLAP OVER COMBINED AREA
+#     iou = area_overlap / (area_combined + epsilon)
+#     return iou
 
 
 def findClosest(time, camera_time_list):
@@ -701,7 +702,7 @@ def extract_frames(path,file_name, model, class_names, width, height, savename, 
                     # bbox = torch.Tensor(bbox)
                     # mx = torch.min(bbox[0], cbbox[0])
 
-                    iou = batch_iou(bbox, cbbox)
+                    iou = get_iou(bbox, cbbox)
                     print("iou")
                     print(iou)
 
