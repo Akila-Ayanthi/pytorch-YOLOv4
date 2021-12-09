3f56093a6e6e2da7634b4802d5dc9a93e6cc5c40
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-09 21:31:32 +0530

    bbox iou

--- models.py
+++ models.py
@@ -713,21 +713,21 @@ def extract_frames(path,file_name, model, class_names, width, height, savename, 
                         print(iou[k][1][0], iou[k][1][1])
                         img = cv2.putText(img, str(iou[k][2]), (iou[k][1][0], iou[k][1][1]), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 1)
 
-                    print("iou")
-                    print(iou)
+                    # print("iou")
+                    # print(iou)
 
-            #     ax[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+                ax[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-            # savepath = "/home/dissana8/LAB/custom_bbox/"+c1_frame_no.split('/')[0]
+            savepath = "/home/dissana8/LAB/custom_bbox/"+c1_frame_no.split('/')[0]
 
-            # if not os.path.exists(savepath):
-            #     os.makedirs(savepath)
+            if not os.path.exists(savepath):
+                os.makedirs(savepath)
 
-            # plt.savefig(savepath+"/"+c1_frame_no.split('/')[-1])
-            # ax[0].cla()
-            # ax[1].cla()
-            # ax[2].cla()
-            # ax[3].cla()
+            plt.savefig(savepath+"/"+c1_frame_no.split('/')[-1])
+            ax[0].cla()
+            ax[1].cla()
+            ax[2].cla()
+            ax[3].cla()
 
         
 
