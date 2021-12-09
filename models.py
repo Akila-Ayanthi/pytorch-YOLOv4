c26491dfb1a1e91a2ec28838253a83cb961b3363
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-09 09:28:35 +0530

    iou calculation

--- models.py
+++ models.py
@@ -593,6 +593,11 @@ def extract_frames(path,file_name, model, class_names, width, height, savename, 
 
                 image, cbbox = custom_bbox(gt[i], img, imgname)
 
+                print(bbox[0])
+                print(cbbox[0])
+                mx = torch.min(bbox[0], cbbox[0])
+                print(mx)
+
                 iou = bbox_ious(bbox, cbbox)
                 print(iou)
 
