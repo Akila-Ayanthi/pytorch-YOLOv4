3505ec05fbdf896b76729b1de03d108067b163f6
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-10 09:07:31 +0530

    bboxiou

--- models.py
+++ models.py
@@ -719,6 +719,7 @@ def extract_frames(path,file_name, model, class_names, width, height, savename, 
                     # print(len(iou))
 
                     for k in range(len(iou)):
+                        print(str(iou[k][1]))
                         print(iou[k][0][0], iou[k][0][1])
                         # img = cv2.putText(img, str(iou[k][1]), (iou[k][0][0], iou[k][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 1.2, (0, 0, 255), 1)
 
