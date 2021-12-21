db799541a470d8d9ee3292ff32ea109113ff28f3
Akila_Ayanthi <aayanthi@gmail.com>
2021-12-21 10:09:02 +0530

    success rate

--- models.py
+++ models.py
@@ -751,93 +751,93 @@ def extract_frames(path,file_name, model, class_names, width, height, savename, 
     counter = -1
     print('Extracting GT annotation ...')
     for line in content:
-        counter += 1
-        if counter % 150 == 0:
-            print(counter)
-            s = line.split(" ")
-            
-            time = float(s[0])
-            frame_idx = findClosest(time, c1_times) # we have to map the time to frame number
-            c1_frame_no = c1_frames[frame_idx]
-            
+        # counter += 1
+        # if counter % 150 == 0:
+            # print(counter)
+        s = line.split(" ")
+        
+        time = float(s[0])
+        frame_idx = findClosest(time, c1_times) # we have to map the time to frame number
+        c1_frame_no = c1_frames[frame_idx]
+        
 
-            frame_idx = findClosest(time, c2_times)  # we have to map the time to frame number
-            c2_frame_no = c2_frames[frame_idx]
-            
+        frame_idx = findClosest(time, c2_times)  # we have to map the time to frame number
+        c2_frame_no = c2_frames[frame_idx]
+        
 
-            frame_idx = findClosest(time, c3_times)  # we have to map the time to frame number
-            c3_frame_no = c3_frames[frame_idx]
-
-            
-            frame_idx = findClosest(time, c4_times)  # we have to map the time to frame number
-            c4_frame_no = c4_frames[frame_idx]
-
-            cam = []
-
-            cam.append('/home/dissana8/LAB/Visor/cam1/'+c1_frame_no)
-            cam.append('/home/dissana8/LAB/Visor/cam2/'+c2_frame_no)
-            cam.append('/home/dissana8/LAB/Visor/cam3/'+c3_frame_no)
-            cam.append('/home/dissana8/LAB/Visor/cam4/'+c4_frame_no)
-
-            f, ax = plt.subplots(1, 4, figsize=(25, 4))
-
-            for i in range(4):
-                img = cv2.imread(cam[i])
-                sized = cv2.resize(img, (width, height))
-                sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
-
-                for j in range(2):  # This 'for' loop is for speed check
-                            # Because the first iteration is usually longer
-                    boxes = do_detect(model, sized, 0.4, 0.6, use_cuda)
-
-                imgfile = cam[i].split('/')[6:]
-                imgname = '/'.join(imgfile)
-                sname = savename + imgname
-
-                img, bbox = plot_boxes_cv2(img, boxes[0], sname, class_names)
-
-                image, cbbox = custom_bbox(gt[i], img, imgname)
-
-                # print(bbox)
-
-                if cbbox:
-                    cbbox = np.array(cbbox)
-                    bbox = np.array(bbox)
-                    idx_gt_actual, idx_pred_actual, ious_actual, label = match_bboxes(cbbox, bbox)
-
-                    for h in range(len(idx_gt_actual)):
-                        t = idx_gt_actual[h]
-                        text_c = cbbox[t]
-                        # print(gt_actual)
-                        if round(ious_actual[h], 3)>=0.0:
-                            print(ious_actual[h])
-                            detections+=1
-                            # img = cv2.putText(img, str(round(ious_actual[h], 3)), (text_c[0], text_c[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-
-                    # iou = get_iou(bbox, cbbox)
-                    # print("iou")
-                    # print(len(iou))
-
-                    # for k in range(len(iou)):
-                    #     img = cv2.putText(img, str(iou[k][1]), (iou[k][0][0], iou[k][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
-
-
-
-            #     ax[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
-
-            # savepath = "/home/dissana8/LAB/custom_bbox/"+c1_frame_no.split('/')[0]
-
-            # if not os.path.exists(savepath):
-            #     os.makedirs(savepath)
-
-            # plt.savefig(savepath+"/"+c1_frame_no.split('/')[-1])
-            # ax[0].cla()
-            # ax[1].cla()
-            # ax[2].cla()
-            # ax[3].cla()
+        frame_idx = findClosest(time, c3_times)  # we have to map the time to frame number
+        c3_frame_no = c3_frames[frame_idx]
 
         
+        frame_idx = findClosest(time, c4_times)  # we have to map the time to frame number
+        c4_frame_no = c4_frames[frame_idx]
+
+        cam = []
+
+        cam.append('/home/dissana8/LAB/Visor/cam1/'+c1_frame_no)
+        cam.append('/home/dissana8/LAB/Visor/cam2/'+c2_frame_no)
+        cam.append('/home/dissana8/LAB/Visor/cam3/'+c3_frame_no)
+        cam.append('/home/dissana8/LAB/Visor/cam4/'+c4_frame_no)
+
+        f, ax = plt.subplots(1, 4, figsize=(25, 4))
+
+        for i in range(4):
+            img = cv2.imread(cam[i])
+            sized = cv2.resize(img, (width, height))
+            sized = cv2.cvtColor(sized, cv2.COLOR_BGR2RGB)
+
+            for j in range(2):  # This 'for' loop is for speed check
+                        # Because the first iteration is usually longer
+                boxes = do_detect(model, sized, 0.4, 0.6, use_cuda)
+
+            imgfile = cam[i].split('/')[6:]
+            imgname = '/'.join(imgfile)
+            sname = savename + imgname
+
+            img, bbox = plot_boxes_cv2(img, boxes[0], sname, class_names)
+
+            image, cbbox = custom_bbox(gt[i], img, imgname)
+
+            # print(bbox)
+
+            if cbbox:
+                cbbox = np.array(cbbox)
+                bbox = np.array(bbox)
+                idx_gt_actual, idx_pred_actual, ious_actual, label = match_bboxes(cbbox, bbox)
+
+                for h in range(len(idx_gt_actual)):
+                    t = idx_gt_actual[h]
+                    text_c = cbbox[t]
+                    # print(gt_actual)
+                    if round(ious_actual[h], 3)>=0.0:
+                        print(ious_actual[h])
+                        detections+=1
+                        # img = cv2.putText(img, str(round(ious_actual[h], 3)), (text_c[0], text_c[1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+
+                # iou = get_iou(bbox, cbbox)
+                # print("iou")
+                # print(len(iou))
+
+                # for k in range(len(iou)):
+                #     img = cv2.putText(img, str(iou[k][1]), (iou[k][0][0], iou[k][0][1]), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+
+
+
+        #     ax[i].imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+
+        # savepath = "/home/dissana8/LAB/custom_bbox/"+c1_frame_no.split('/')[0]
+
+        # if not os.path.exists(savepath):
+        #     os.makedirs(savepath)
+
+        # plt.savefig(savepath+"/"+c1_frame_no.split('/')[-1])
+        # ax[0].cla()
+        # ax[1].cla()
+        # ax[2].cla()
+        # ax[3].cla()
+
+    
 
 
 def box_center_to_corner(boxes):
