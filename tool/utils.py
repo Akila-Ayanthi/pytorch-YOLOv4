47eba940eaf24d93b3ab68ea824164b25d92b7cc
Akila_Ayanthi <aayanthi@gmail.com>
2022-04-11 10:09:40 +0530

    placing patches on detections

--- tool/utils.py
+++ tool/utils.py
@@ -122,7 +122,6 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
 
         coords = [x1, y1, x2, y2]
 
-        bbox_coords.append(coords)
 
         if color:
             rgb = color
@@ -148,6 +147,7 @@ def plot_boxes_cv2(img, boxes, savename=None, class_names=None, color=None):
             # print('%s: %f' % (class_names[cls_id], cls_conf))
 #             img = cv2.putText(img, class_names[cls_id], (x1, y1), cv2.FONT_HERSHEY_SIMPLEX, 1.2, rgb, 1)
             img = cv2.rectangle(img, (x1, y1), (x2, y2), rgb, 2)
+            bbox_coords.append(coords)
 
     # if savename:
     #     print("save plot results to %s" % savename)
