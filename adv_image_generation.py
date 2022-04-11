3972a87a1cf1f026959defed17b558b281566fa3
Akila_Ayanthi <aayanthi@gmail.com>
2022-04-11 10:40:34 +0530

    placing patches on detections

--- adv_image_generation.py
+++ adv_image_generation.py
@@ -1211,7 +1211,7 @@ def extract_roi(detections, class_id, img_bbox, min_size, patch_size):
 
 def single_image_det(height, width):
     patch = cv2.imread("/home/dissana8/Daedalus-physical/physical_examples/0.5 confidence/epoch8-iter4000-cw tanh perturbation.png")
-    resized_patch = cv2.resize(patch, (24, 24))
+    resized_patch = cv2.resize(patch, (16, 16))
     im = "/home/dissana8/LAB/Visor/cam1/000005/005614.jpg"
     img = cv2.imread(im)
     sized = cv2.resize(img, (width, height))
@@ -1252,10 +1252,10 @@ def single_image_det(height, width):
         # print(y)
 
     #     print(replace[y-8: y +8, x-8 : x + 8].shape)
-        if (y+12)>=480 or (x+12)>=640 or (x-12)<0 or (y-12)<0:
+        if (y+8)>=480 or (x+8)>=640 or (x-8)<0 or (y-8)<0:
             continue
         else:
-            replace[y-12: y +12, x-12 : x + 12] = resized_patch
+            replace[y-8: y +8, x-8 : x + 8] = resized_patch
     # replace = cv2.cvtColor(replace, cv2.COLOR_RGB2BGR)
     cv2.imwrite('boxed.png', img)
     cv2.imwrite('replace_.png', replace)
