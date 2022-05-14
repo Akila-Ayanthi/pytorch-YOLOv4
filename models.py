0db55125c7304c1f88f50e38f7098b96ff0cc4b8
Akila_Ayanthi <aayanthi@gmail.com>
2022-05-14 10:36:21 +0530

    downsizing gt coords

--- models.py
+++ models.py
@@ -1168,7 +1168,7 @@ def box_center_to_corner(boxes):
     x1 = cx - 0.5 * w
     y1 = cy - 0.8 * h
     x2 = cx + 0.5 * w
-    y2 = cy + 0.2 * h
+    y2 = cy + 0.8 * h
     boxes = torch.stack((x1, y1, x2, y2), axis=-1)
     return boxes
 
